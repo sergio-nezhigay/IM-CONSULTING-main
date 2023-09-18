ed0128ecdb74510491fcba2e33caaa11110595eb
Sergio Nezhigay <abc1971h@gmail.com>
2023-09-18 19:29:26 +0300

    q70

--- src/components/HeroContent/HeroContent.jsx
+++ src/components/HeroContent/HeroContent.jsx
@@ -48,7 +48,7 @@ export const HeroContent = () => {
           src={HeroImg}
           alt={`${name} ${surname}`}
           priority
-          quality={100}
+          quality={70}
           style={{ width: '100%', height: 'auto' }}
         />
       </div>
