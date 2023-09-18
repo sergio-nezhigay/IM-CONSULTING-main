f0e7c499778056e5c1812e0693013ac4e1de11a8
Sergio Nezhigay <abc1971h@gmail.com>
2023-09-18 20:29:18 +0300

    rev

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,8 +2,8 @@ import { Hero } from '@/sections/Hero';
 import { AboutCompany } from '@/sections/AboutCompany';
 import { Services } from '@/sections/Services';
 import { Process } from '@/sections/Process';
-// import { Partners } from '@/sections/Partners';
-import { Reviews } from '@/sections/Reviews';
+import { Partners } from '@/sections/Partners';
+// import { Reviews } from '@/sections/Reviews';
 import { Contacts } from '@/sections/Contacts';
 
 export default async function Home() {
@@ -13,8 +13,8 @@ export default async function Home() {
       <AboutCompany />
       <Services />
       <Process />
-      {/*   <Partners />*/}
-      <Reviews />
+      <Partners />
+      {/*   <Reviews />*/}
       <Contacts />
     </>
   );
